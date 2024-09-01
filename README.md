1b9a1e10fe57775f8d661b1d36868e1fba15aecf
Paul Berberian <paul.berberian+github@proton.me>
2024-09-01 14:08:06 +0200

    Update LazyVim config

---
+++ README.md
@@ -0,0 +1,12 @@
+# dotfiles
+
+NOTES:
+
+  - `_home_/.config/nvim` for now rely on LazyVim being installed and only
+    contains my own supplementary files, not the full config LazyVim installs.
+
+    `_home_/.config/nvim.back` is a different neovim config that does not rely
+    on LazyVim.
+
+  - Many dotfiles may be out-of-date, I distro-hopped, wm-hopped, editor-hopped
+    and tools-hopped a lot.
--- _home_/.config/nvim/lua/plugins/config.lua
+++ _home_/.config/nvim/lua/plugins/config.lua
@@ -4,40 +4,40 @@
 -- vim.keymap.set("n", "<LEADER>M", ":MarkClear<CR>:nohl<CR>", { silent = true })
 
 return {
-  {
-    -- vim-eunuch: various UNIX tools
-    "tpope/vim-eunuch",
-  },
-  -- {
-  --   -- vim-mark: Mark and unmark words
-  --   "inkarkat/vim-ingo-library",
-  --   "inkarkat/vim-mark",
-  -- },
-  {
-    "stevearc/conform.nvim",
-    opts = function()
-      local opts = {
-        formatters_by_ft = {
-          lua = { "stylua" },
-          fish = { "fish_indent" },
-          sh = { "shfmt" },
-          typescript = { "prettier" },
-          rust = { "rustfmt" },
-        },
-        formatters = {
-          -- # Example of using shfmt with extra args
-          shfmt = {
-            prepend_args = { "-i", "2" },
-          },
-        },
-      }
-      return opts
-    end,
-  },
-  {
-    "rcarriga/nvim-notify",
-    opts = {
-      timeout = 8000,
-    },
-  },
+	{
+		-- vim-eunuch: various UNIX tools
+		"tpope/vim-eunuch",
+	},
+	-- {
+	--   -- vim-mark: Mark and unmark words
+	--   "inkarkat/vim-ingo-library",
+	--   "inkarkat/vim-mark",
+	-- },
+	{
+		"stevearc/conform.nvim",
+		opts = function()
+			local opts = {
+				formatters_by_ft = {
+					lua = { "stylua" },
+					fish = { "fish_indent" },
+					sh = { "shfmt" },
+					typescript = { "prettier" },
+					rust = { "rustfmt" },
+				},
+				formatters = {
+					-- # Example of using shfmt with extra args
+					shfmt = {
+						prepend_args = { "-i", "2" },
+					},
+				},
+			}
+			return opts
+		end,
+	},
+	{
+		"rcarriga/nvim-notify",
+		opts = {
+			timeout = 8000,
+		},
+	},
 }
