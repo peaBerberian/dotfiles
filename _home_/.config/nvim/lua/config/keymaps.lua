b9c60adcb8ae940887a8d399d9f66de7eab8649c
Paul Berberian <paul.berberian+github@proton.me>
2024-09-15 01:20:52 +0200

    nvim: add keymaps

---
+++ _home_/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,37 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local silent_and_nowait = { silent = true, nowait = true }
+
+-- jk to Escape
+vim.keymap.set("i", "jk", [[<C-\><C-n>]])
+vim.keymap.set("i", "kj", [[<C-\><C-n>]])
+
+-- Buffer navigation
+vim.keymap.set("n", "<C-j>", ":bprevious!<CR>", silent_and_nowait)
+vim.keymap.set("n", "<C-S-Tab>", ":bprevious!<CR>", silent_and_nowait)
+vim.keymap.set("n", "<C-k>", ":bnext!<CR>", silent_and_nowait)
+vim.keymap.set("n", "<C-Tab>", ":bnext!<CR>", silent_and_nowait)
+
+-- Tab to indent
+vim.keymap.set("n", "<Tab>", ">>", silent_and_nowait)
+vim.keymap.set("n", "<S-Tab>", "<<", silent_and_nowait)
+vim.keymap.set("v", "<Tab>", ">gv", silent_and_nowait)
+vim.keymap.set("v", "<S-Tab>", "<gv", silent_and_nowait)
+
+-- Save buffer
+-- vim.keymap.set("n", "<LEADER>s", ":w<CR>", silent_and_nowait)
+vim.keymap.set("n", "<C-s>", ":w<CR>", silent_and_nowait)
+vim.keymap.set("i", "<C-s>", "<ESC>:w<CR>a", silent_and_nowait)
+
+-- Copy in clipboard with CTRL-c on visual mode
+vim.keymap.set("v", "<C-c>", '"+y', silent_and_nowait)
+
+-- F2 Toggle inlay hints
+-- vim.keymap.set(
+-- 	"n",
+-- 	"<F2>",
+-- 	":lua vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())<CR>",
+-- 	silent_and_nowait
+-- )
