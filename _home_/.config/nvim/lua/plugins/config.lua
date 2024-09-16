afb0255f19a6bbbfebb8c23b8da6515ce419aeb8
Paul Berberian <paul.berberian+github@proton.me>
2024-09-16 12:56:01 +0200

    nvim: oil replace netrw

--- _home_/.config/nvim/lua/plugins/config.lua
+++ _home_/.config/nvim/lua/plugins/config.lua
@@ -9,6 +9,19 @@ return {
 		"peaBerberian/Mark--Karkat",
 	},
 	{
+		"stevearc/oil.nvim",
+		opts = {
+			-- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
+			-- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
+			default_file_explorer = true,
+			columns = {
+				"icon",
+				"size",
+				"mtime",
+			},
+		},
+	},
+	{
 		"stevearc/conform.nvim",
 		opts = function()
 			local opts = {
