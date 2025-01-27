2a1fcb51ad22e74b292f603090f855b4b2cbac8f
Paul Berberian <paul.berberian@proton.me>
2025-01-27 21:46:16 +0100

    nvim: rely on more ""minimalist"" nvim config, lazyvim is too much sometimes

---
+++ _home_/.config/nvim/lua/keymaps.lua
@@ -0,0 +1,126 @@
+local silent_and_nowait = { silent = true, nowait = true }
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- jk to Escape
+vim.keymap.set('i', 'jk', [[<C-\><C-n>]])
+vim.keymap.set('i', 'kj', [[<C-\><C-n>]])
+
+-- Buffer navigation
+vim.keymap.set('n', '<C-j>', ':bprevious!<CR>', silent_and_nowait)
+vim.keymap.set('n', '<C-S-Tab>', ':bprevious!<CR>', silent_and_nowait)
+vim.keymap.set('n', '<C-k>', ':bnext!<CR>', silent_and_nowait)
+vim.keymap.set('n', '<C-Tab>', ':bnext!<CR>', silent_and_nowait)
+
+vim.keymap.set('n', '<LEADER>bd', ':bd!<CR>', {
+  silent = true,
+  nowait = true,
+  desc = 'Close current buffer',
+})
+
+-- split navigation
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+
+vim.keymap.set('n', '<LEADER>fb', ':Telescope buffers<CR>', {
+  silent = true,
+  nowait = true,
+  desc = 'Find buffers',
+})
+vim.keymap.set('n', '<LEADER>ff', ':Telescope find_files<CR>', {
+  silent = true,
+  nowait = true,
+  desc = 'Find files',
+})
+vim.keymap.set('n', '<LEADER>fg', ':Telescope git_files<CR>', {
+  silent = true,
+  nowait = true,
+  desc = 'Find git files',
+})
+
+-- Tab to indent
+vim.keymap.set('n', '<Tab>', '>>', silent_and_nowait)
+vim.keymap.set('n', '<S-Tab>', '<<', silent_and_nowait)
+vim.keymap.set('v', '<Tab>', '>gv', silent_and_nowait)
+vim.keymap.set('v', '<S-Tab>', '<gv', silent_and_nowait)
+
+-- Save buffer
+-- vim.keymap.set("n", "<LEADER>s", ":w<CR>", silent_and_nowait)
+vim.keymap.set('n', '<C-s>', ':w<CR>', silent_and_nowait)
+vim.keymap.set('i', '<C-s>', '<ESC>:w<CR>a', silent_and_nowait)
+
+-- Copy in clipboard with CTRL-c on visual mode
+vim.keymap.set('v', '<C-c>', '"+y', silent_and_nowait)
+
+-- Marks
+vim.keymap.set('n', '<LEADER>m', '<Plug>MarkSet', {
+  silent = true,
+  nowait = true,
+  desc = 'Highlight occurences (word under cursor)',
+})
+vim.keymap.set('v', '<LEADER>m', '<Plug>MarkSet', {
+  silent = true,
+  nowait = true,
+  desc = 'Highlight occurences (selection)',
+})
+vim.keymap.set('n', '<LEADER>M', '<Plug>MarkClear', {
+  silent = true,
+  nowait = true,
+  desc = 'Clear all highlights',
+})
+
+vim.keymap.set('n', '<LEADER>d', '<c-w>v<cmd>lua vim.lsp.buf.definition()<CR>', {
+  silent = true,
+  nowait = true,
+  desc = 'Go to definition in a vertical split',
+})
+
+-- Yank ring iterations
+vim.keymap.set({ 'n', 'x' }, 'p', '<Plug>(YankyPutAfter)')
+vim.keymap.set({ 'n', 'x' }, 'P', '<Plug>(YankyPutBefore)')
+vim.keymap.set({ 'n', 'x' }, 'gp', '<Plug>(YankyGPutAfter)')
+vim.keymap.set({ 'n', 'x' }, 'gP', '<Plug>(YankyGPutBefore)')
+vim.keymap.set('n', '<c-p>', '<Plug>(YankyPreviousEntry)')
+vim.keymap.set('n', '<c-n>', '<Plug>(YankyNextEntry)')
+
+vim.keymap.set('n', '<LEADER>a', "<cmd>echo expand('%')<CR>", {
+  nowait = true,
+  desc = 'Print current filename',
+})
+
+vim.keymap.set('n', '<LEADER>xt', ':lua vim.diagnostic.open_float()<CR>', {
+  silent = true,
+  nowait = true,
+  desc = 'Open diagnostics for current line in float',
+})
+
+-- Open terminal
+vim.keymap.set('n', '<LEADER>t', ':terminal<CR>a', {
+  silent = true,
+  nowait = true,
+  desc = 'Open terminal',
+})
+
+-- Go in normal mode in terminal
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+vim.keymap.set('t', 'jk', '<C-\\><C-n>', {
+  silent = true,
+  nowait = true,
+})
+vim.keymap.set('t', 'kj', '<C-\\><C-n>', {
+  silent = true,
+  nowait = true,
+})
+
+-- F2 Toggle inlay hints
+-- vim.keymap.set(
+-- 	"n",
+-- 	"<F2>",
+-- 	":lua vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())<CR>",
+-- 	silent_and_nowait
+-- )
---
+++ _home_/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,327 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+
+return {
+  -- 'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+  -- Highlight todo, notes, etc in comments
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      signs = false,
+    },
+  },
+  { -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl',
+    opts = {},
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    -- Optional dependency
+    dependencies = { 'hrsh7th/nvim-cmp' },
+    config = function()
+      require('nvim-autopairs').setup {}
+      -- If you want to automatically add `(` after selecting a function or method
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+      local cmp = require 'cmp'
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end,
+  },
+  { -- Useful plugin to show you pending keybinds.
+    'folke/which-key.nvim',
+    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+    opts = {
+      -- delay between pressing a key and opening which-key (milliseconds)
+      -- this setting is independent of vim.opt.timeoutlen
+      delay = 0,
+      icons = {
+        -- set icon mappings to true if you have a Nerd Font
+        mappings = vim.g.have_nerd_font,
+        -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
+        -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
+        keys = vim.g.have_nerd_font and {} or {
+          Up = '<Up> ',
+          Down = '<Down> ',
+          Left = '<Left> ',
+          Right = '<Right> ',
+          C = '<C-…> ',
+          M = '<M-…> ',
+          D = '<D-…> ',
+          S = '<S-…> ',
+          CR = '<CR> ',
+          Esc = '<Esc> ',
+          ScrollWheelDown = '<ScrollWheelDown> ',
+          ScrollWheelUp = '<ScrollWheelUp> ',
+          NL = '<NL> ',
+          BS = '<BS> ',
+          Space = '<Space> ',
+          Tab = '<Tab> ',
+          F1 = '<F1>',
+          F2 = '<F2>',
+          F3 = '<F3>',
+          F4 = '<F4>',
+          F5 = '<F5>',
+          F6 = '<F6>',
+          F7 = '<F7>',
+          F8 = '<F8>',
+          F9 = '<F9>',
+          F10 = '<F10>',
+          F11 = '<F11>',
+          F12 = '<F12>',
+        },
+      },
+
+      -- Document existing key chains
+      spec = {
+        { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<LEADER>f', group = '[F]ind' },
+        { '<LEADER>b', group = '[B]uffer' },
+        { '<LEADER>g', group = '[G]it' },
+        { '<LEADER>t', desc = '[T]erminal' },
+      },
+    },
+  },
+  {
+    'gbprod/yanky.nvim',
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      system_clipboard = {
+        sync_with_ring = false,
+      },
+      highlight = {
+        on_put = true,
+        on_yank = true,
+        timer = 250,
+      },
+    },
+  },
+
+  {
+    'folke/trouble.nvim',
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
+    keys = {
+      {
+        '<leader>cx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>cX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Troble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>cL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>cQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
+    },
+  },
+  -- {
+  --   'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
+  -- },
+  {
+    -- vim-eunuch: various UNIX tools
+    'tpope/vim-eunuch',
+  },
+  {
+    'tpope/vim-fugitive',
+    keys = {
+      { '<leader>gd', '<cmd>Gdiffsplit<cr>', desc = 'Git diff in a split' },
+      { '<leader>gb', '<cmd>Git blame<cr>', desc = 'Git blame' },
+    },
+  },
+  {
+    'akinsho/bufferline.nvim',
+    opts = {
+      options = {
+        always_show_bufferline = true,
+        middle_mouse_command = 'bdelete! %d', -- can be a string | function | false, see "Mouse actions"
+        diagnostics = 'nvim_lsp',
+        diagnostics_indicator = function(count)
+          return '(' .. count .. ')'
+        end,
+      },
+    },
+  },
+  {
+    -- vim-mark: Mark and unmark words
+    'inkarkat/vim-ingo-library',
+    'peaBerberian/Mark--Karkat',
+  },
+  {
+    'stevearc/oil.nvim',
+    opts = {
+      -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
+      -- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
+      default_file_explorer = true,
+      columns = {
+        'icon',
+        'size',
+        'mtime',
+      },
+      view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+      },
+    },
+  },
+  {
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>cf',
+        function()
+          require('conform').format { async = true, lsp_format = 'fallback' }
+        end,
+        mode = '',
+        desc = '[F]ormat buffer',
+      },
+    },
+    opts = function()
+      local opts = {
+        notify_on_error = false,
+        formatters_by_ft = {
+          lua = { 'stylua' },
+          fish = { 'fish_indent' },
+          sh = { 'shfmt' },
+          typescript = { 'prettier' },
+          javascript = { 'prettierd', 'prettier', stop_after_first = true },
+          javascriptreact = { 'prettier' },
+          typescriptreact = { 'prettier' },
+          markdown = { 'prettier' },
+          rust = { 'rustfmt' },
+        },
+        formatters = {
+          -- # Example of using shfmt with extra args
+          shfmt = {
+            prepend_args = { '-i', '2' },
+          },
+        },
+        -- format_on_save = {
+        --   -- These options will be passed to conform.format()
+        --   timeout_ms = 500,
+        --   lsp_format = 'fallback',
+        -- },
+        format_on_save = function(bufnr)
+          -- Disable "format_on_save lsp_fallback" for languages that don't
+          -- have a well standardized coding style. You can add additional
+          -- languages here or re-enable it for the disabled ones.
+          local disable_filetypes = { c = true, cpp = true }
+          local lsp_format_opt
+          if disable_filetypes[vim.bo[bufnr].filetype] then
+            lsp_format_opt = 'never'
+          else
+            lsp_format_opt = 'fallback'
+          end
+          return {
+            timeout_ms = 500,
+            lsp_format = lsp_format_opt,
+          }
+        end,
+      }
+      return opts
+    end,
+  },
+  { -- Linting
+    'mfussenegger/nvim-lint',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      local lint = require 'lint'
+      lint.linters_by_ft = {
+        markdown = { 'markdownlint' },
+        typescript = { 'eslint' },
+        javascript = { 'eslint', stop_after_first = true },
+        javascriptreact = { 'eslint' },
+        typescriptreact = { 'eslint' },
+      }
+
+      -- To allow other plugins to add linters to require('lint').linters_by_ft,
+      -- instead set linters_by_ft like this:
+      -- lint.linters_by_ft = lint.linters_by_ft or {}
+      -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
+      --
+      -- However, note that this will enable a set of default linters,
+      -- which will cause errors unless these tools are available:
+      -- {
+      --   clojure = { "clj-kondo" },
+      --   dockerfile = { "hadolint" },
+      --   inko = { "inko" },
+      --   janet = { "janet" },
+      --   json = { "jsonlint" },
+      --   markdown = { "vale" },
+      --   rst = { "vale" },
+      --   ruby = { "ruby" },
+      --   terraform = { "tflint" },
+      --   text = { "vale" }
+      -- }
+      --
+      -- You can disable the default linters by setting their filetypes to nil:
+      -- lint.linters_by_ft['clojure'] = nil
+      -- lint.linters_by_ft['dockerfile'] = nil
+      -- lint.linters_by_ft['inko'] = nil
+      -- lint.linters_by_ft['janet'] = nil
+      -- lint.linters_by_ft['json'] = nil
+      -- lint.linters_by_ft['markdown'] = nil
+      -- lint.linters_by_ft['rst'] = nil
+      -- lint.linters_by_ft['ruby'] = nil
+      -- lint.linters_by_ft['terraform'] = nil
+      -- lint.linters_by_ft['text'] = nil
+
+      -- Create autocommand which carries out the actual linting
+      -- on the specified events.
+      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+        group = lint_augroup,
+        callback = function()
+          -- Only run the linter in buffers that you can modify in order to
+          -- avoid superfluous noise, notably within the handy LSP pop-ups that
+          -- describe the hovered symbol using Markdown.
+          if vim.opt_local.modifiable:get() then
+            lint.try_lint()
+          end
+        end,
+      })
+    end,
+  },
+}
