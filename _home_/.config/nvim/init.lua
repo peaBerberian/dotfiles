55464b592000fe19b37a024be4fc5bb6a8be62cf
Paul Berberian <paul.berberian@proton.me>
2026-02-14 22:23:42 +0100

    nvim: treesitter simplification

--- _home_/.config/nvim/init.lua
+++ _home_/.config/nvim/init.lua
@@ -488,55 +488,8 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = {
-        'bash',
-        'c',
-        'cpp',
-        'css',
-        'diff',
-        'elixir',
-        'dockerfile',
-        'erlang',
-        'fish',
-        'go',
-        'html',
-        'javascript',
-        'json',
-        'json5',
-        'kotlin',
-        'lua',
-        'luadoc',
-        'markdown',
-        'markdown_inline',
-        'nix',
-        'ocaml',
-        'php',
-        'python',
-        'query',
-        'regex',
-        'rust',
-        'toml',
-        'tsx',
-        'typescript',
-        'vim',
-        'vimdoc',
-        'zig',
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
   { 'ziglang/zig.vim' },
   {
